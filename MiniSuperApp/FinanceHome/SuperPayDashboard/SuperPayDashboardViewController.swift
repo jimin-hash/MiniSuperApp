c37f1120243d519b4652c90a9eeb94590c87f081
jimin-hash <jimin.park2125@gmail.com>
2024-11-03 16:25:17 +0900

    Initial Commit

---
+++ MiniSuperApp/FinanceHome/SuperPayDashboard/SuperPayDashboardViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SuperPayDashboardViewController.swift
+//  MiniSuperApp
+//
+//  Created by Jimin Park on 10/20/24.
+//
+
+import ModernRIBs
+import UIKit
+
+protocol SuperPayDashboardPresentableListener: AnyObject {
+    // TODO: Declare properties and methods that the view controller can invoke to perform
+    // business logic, such as signIn(). This protocol is implemented by the corresponding
+    // interactor class.
+}
+
+final class SuperPayDashboardViewController: UIViewController, SuperPayDashboardPresentable, SuperPayDashboardViewControllable {
+
+    weak var listener: SuperPayDashboardPresentableListener?
+}
