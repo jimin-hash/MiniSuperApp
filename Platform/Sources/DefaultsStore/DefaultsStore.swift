619e3e8f9f93a510cf220611be0962b8aa4b2165
jimin-hash <jimin.park2125@gmail.com>
2024-11-29 01:56:48 +0900

    Feat: DefaultsStore OCP 준수하도록 적용

--- Platform/Sources/DefaultsStore/DefaultsStore.swift
+++ Platform/Sources/DefaultsStore/DefaultsStore.swift
@@ -1,37 +1,61 @@
 import Foundation
 
+// OCP 준수 X
+//public protocol DefaultsStore {
+//    var isInitialLaunch: Bool { get set }
+//    var lastNoticeDate: Double { get set }
+//}
+//
+//public struct DefaultsStoreImp: DefaultsStore {
+//    
+//    public var isInitialLaunch: Bool {
+//        get {
+//            userDefaults.bool(forKey: kIsInitialLaunch)
+//        }
+//        set {
+//            userDefaults.set(newValue, forKey: kIsInitialLaunch)
+//        }
+//    }
+//    
+//    public var lastNoticeDate: Double {
+//        get {
+//            userDefaults.double(forKey: kLastNoticeDate)
+//        }
+//        set { 
+//            userDefaults.set(newValue, forKey: kLastNoticeDate)
+//        }
+//    }
+//    
+//    private let userDefaults: UserDefaults
+//    
+//    private let kIsInitialLaunch = "kIsInitialLaunch"
+//    private let kLastNoticeDate = "kLastNoticeDate"
+//    
+//    public init(defaults: UserDefaults) {
+//        self.userDefaults = defaults
+//    }
+//    
+//}
+
+// OCP 준수 O
 public protocol DefaultsStore {
-    var isInitialLaunch: Bool { get set }
-    var lastNoticeDate: Double { get set }
+    func get<T>(key: String, defaultValue: T) -> T
+    func set<T>(key: String, value: T)
 }
 
 public struct DefaultsStoreImp: DefaultsStore {
-    
-    public var isInitialLaunch: Bool {
-        get {
-            userDefaults.bool(forKey: kIsInitialLaunch)
-        }
-        set {
-            userDefaults.set(newValue, forKey: kIsInitialLaunch)
-        }
-    }
-    
-    public var lastNoticeDate: Double {
-        get {
-            userDefaults.double(forKey: kLastNoticeDate)
-        }
-        set { 
-            userDefaults.set(newValue, forKey: kLastNoticeDate)
-        }
-    }
-    
     private let userDefaults: UserDefaults
-    
-    private let kIsInitialLaunch = "kIsInitialLaunch"
-    private let kLastNoticeDate = "kLastNoticeDate"
-    
+
     public init(defaults: UserDefaults) {
         self.userDefaults = defaults
     }
-    
+
+    public func get<T>(key: String, defaultValue: T) -> T {
+        return userDefaults.object(forKey: key) as? T ?? defaultValue
+    }
+
+    public func set<T>(key: String, value: T) {
+        userDefaults.set(value, forKey: key)
+    }
 }
+
